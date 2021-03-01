1ebc7a0089001984e44b5d9ae8ea53a41367da9c
josh <josh.fierrosga@gmail.com>
2021-02-28 22:05:19 -0600

    api script for library of congress api

---
+++ loc-script.py
@@ -0,0 +1,31 @@
+# New to Python? ->
+# To run this file open a terminal here [CTRL + ~ ]
+# then type: "python3 [name of your file].py" then press ENTER
+
+# https://libraryofcongress.github.io/data-exploration/
+# https://blogs.loc.gov/thesignal/2020/11/lc-for-robots-in-action-using-the-api-to-access-the-federal-theatre-project-collection/
+import requests
+import pprint
+import re
+import os
+
+urls = ['usteledirec04774x']
+iterator = 0
+for link in urls:
+    os.makedirs(f'../Desktop/{link}')
+    response = requests.get(f"https://www.loc.gov/item/{link}?fo=json").json()
+
+    for file in response['resources'][0]['files']:
+
+        for obj in file:
+            pprint.pprint(obj)
+            print('\n')
+            if 'info' in obj:
+                if obj['mimetype'] == 'image/jp2':
+                    image_response = requests.get(obj['url'])
+                    iterator = iterator + 1
+                    with open(f'../Desktop/{link}/{link}{iterator}.jpeg', 'wb') as fd:
+                        fd.write(image_response.content)
+                        break
+                else:
+                    print("No file found or wrong data type")
